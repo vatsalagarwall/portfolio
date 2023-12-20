f9e35e76735237ea953cac2a75a67fc80aaee129
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-20 12:26:36 +0530

    commit

--- client/src/Components/Contact.jsx
+++ client/src/Components/Contact.jsx
@@ -223,3 +223,9 @@ function Contact() {
 }
 
 export default Contact;
+
+// "scripts": {
+//   "start": "node index.js",
+//   "dev": "nodemon index.js",
+//   "test": "echo 'Error: no test specified' && exit 1"
+// },
